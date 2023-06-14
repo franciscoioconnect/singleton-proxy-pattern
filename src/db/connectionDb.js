aabb0caa95f428c94b445d8931f9a16587ed276e
Francisco Delgado <francisco.delgado@ioconnectservices.com>
2023-06-14 13:14:59 -0600

    - create class to connect to DB with sequelize

---
+++ src/db/connectionDb.js
@@ -0,0 +1,27 @@
+import { Sequelize } from "sequelize";
+
+let instance;
+
+class DBConnection {
+    constructor() {
+        if (instance) {
+            throw new Error("You can only create one DB Connection!");
+        }
+        this.sequelize = new Sequelize("sqlite::memory:");
+        instance = this;
+    }
+    async connectDB() {
+        try {
+            await this.sequelize.authenticate();
+            await this.sequelize.sync();
+            console.log("db is online");
+        } catch (error) {
+            console.log("Error in db connection");
+            console.log({ error });
+        }
+    }
+}
+
+let dbConnection = Object.freeze(new DBConnection());
+
+export default dbConnection;
